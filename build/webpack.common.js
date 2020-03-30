d343f52abe9d81d7539d1534fdd21cc4d7bd7b07
Kuss <2360314753@qq.com>
2020-03-30 21:01:38 +0800

    :tada: initial

---
+++ build/webpack.common.js
@@ -0,0 +1,86 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, '../src/index.tsx'),
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'js/[name].[hash:8].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          { loader: 'cache-loader' },
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+              experimentalWatchApi: true,
+              happyPackMode: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/index.html')
+    }),
+    new CopyPlugin([
+      {
+        from: path.resolve(__dirname, '../assets'),
+        to: 'assets'
+      }
+    ])
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '../src/')
+    },
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.png']
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1]
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace('@', '')}`
+          }
+        }
+      }
+    }
+  }
+}
