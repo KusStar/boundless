b7d763b5dc422a2a7c39e575f2e2c87b4a59d5b9
Kuss <2360314753@qq.com>
2020-03-31 15:13:34 +0800

    :lipstick: update: use Reset from 'styled-reset'

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Reset } from 'styled-reset'
+import Home from '@pages/Home'
 
 const App: React.FC = () => {
   return (
-    <h1>nmsl</h1>
+    <>
+      <Reset />
+      <Home />
+    </>
   )
 }
 export default App
