717c722da7cd292e3019fefc091f5cf201d252fe
Kuss <2360314753@qq.com>
2020-03-31 15:14:58 +0800

    :lipstick: update: @components/core -> Container, Text

---
+++ src/components/core/Container.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+interface Props {
+  fullscreen?: boolean;
+  background?: string;
+}
+
+const Container = styled.div<Props>`
+  height: ${props => props.fullscreen ? '100vh' : '100%'};
+  width: ${props => props.fullscreen ? '100vw' : '100%'};
+  background: '#fff';
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export default Container
---
+++ src/components/core/Text.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Text = styled.p`
+  color: #fff;
+  font-size: 1.5rem;
+  font-family: 'Noto Serif TC', serif;
+  white-space: pre-line;
+  line-height: 2rem;
+`
+
+export default Text
