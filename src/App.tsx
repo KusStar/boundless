4c218dbc296b57dcfb21461b295d5b1e4de4975f
Kuss <2360314753@qq.com>
2020-04-01 10:56:44 +0800

    :lipstick: update: use DefaultStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Reset } from 'styled-reset'
+import { DefaultStyles } from '@utils/default-styles'
 import Home from '@pages/Home'
 
 const App: React.FC = () => {
   return (
     <>
       <Reset />
+      <DefaultStyles />
       <Home />
     </>
   )
