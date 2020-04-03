a4c54827f4a094a400ff35043708013416bfa2ff
Kuss <2360314753@qq.com>
2020-04-03 21:27:43 +0800

    :lipstick: update: @components/Box

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -4,8 +4,6 @@ import { mobileWidthPixels } from '@utils/threshold'
 
 const Box = styled(Container)`
   width: 400px;
-  background: #fff;
-  transition: all 0.3s ease-in-out;
 
   @media (max-width: ${mobileWidthPixels}) {
     width: 100%;
