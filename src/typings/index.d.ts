bec2acd2905fd06227bc6ffeb0c225131dd4e38f
Kuss <2360314753@qq.com>
2020-04-09 13:00:02 +0800

    :label: add: typings

---
+++ src/typings/index.d.ts
@@ -0,0 +1,48 @@
+
+declare interface System {
+  time: string
+  scene: {
+    id: number
+    text: string
+  }
+}
+
+declare interface Player extends PlayerEffectableProps {
+  name: string
+}
+
+declare interface Event {
+  // 事件 ID
+  id: number
+  // 事件名称
+  name: string
+  // 事件描述、引叙文字
+  description: string
+  // 事件选项
+  options: EventOption[]
+}
+
+interface PlayerEffectableProps {
+  health: number
+  energy: number
+  mood: number
+  food: number
+  money: number
+  mask: number
+}
+
+interface EventOption {
+  id: number
+  name: string
+  // 与之关联的事件, 可选
+  next?: Event
+  // 选择后带来的影响
+  effect: EventEffect
+}
+
+interface EventEffect {
+  flag: number
+  result: PlayerEffectableProps
+}
+
+
