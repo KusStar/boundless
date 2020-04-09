2f06e1ee924f514054d600e790a7c29135bbe448
Kuss <2360314753@qq.com>
2020-04-09 15:57:35 +0800

    :bento: add: assets

---
+++ src/utils/assets.ts
@@ -0,0 +1,13 @@
+const home = 'assets/background/home.jpg'
+const morning = 'assets/images/morning.png'
+const noon = 'assets/images/noon.png'
+const night = 'assets/images/night.png'
+const property = 'assets/images/property.png'
+
+export default {
+  home,
+  morning,
+  noon,
+  night,
+  property
+}
