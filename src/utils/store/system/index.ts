09ed23fbe3cab8eb617be02964c2729cdeaeadb7
Kuss <2360314753@qq.com>
2020-04-10 15:50:20 +0800

    :art: improve: @utils/store/system

--- src/utils/store/system/index.ts
+++ src/utils/store/system/index.ts
@@ -1,7 +1,8 @@
+import dayjs from 'dayjs'
 
 export default { 
   state: {
-    time: '0',
+    time: 1579838400000,
     scene: {
       id: -1,
       text: ''
@@ -20,10 +21,21 @@ export const actions = {
         }
       }
     }
+  },
+  updateTime: ({ system }: State) => {
+    const oldTime = dayjs(system.time)
+    const newTime = oldTime.add(1, 'hour')
+    return {
+      system: {
+        ...system,
+        time: newTime.unix() * 1000
+      }
+    }
   }
 }
 
 export interface InjectedSystemProps {
   system: System,
   onScene: (id: number) => void
+  updateTime: () => void
 }
