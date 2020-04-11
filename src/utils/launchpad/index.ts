5794820757f9781b8abcc90508a94e4c276beb7b
Kuss <2360314753@qq.com>
2020-04-11 10:02:03 +0800

    :sparkles: feat: add @utils/launchpad

---
+++ src/utils/launchpad/index.ts
@@ -0,0 +1,51 @@
+import resources from './resources'
+
+const audio = new Audio()
+const bgmAudio = new Audio()
+
+const fire = (sound: Sound) => {
+  const source = resources[sound]
+  if (!source) return
+
+  let times = source.times - 1
+
+  audio.src = source.src
+  audio.play()
+
+  audio.onended = () => {
+    if (times > 0) {
+      times -= 1
+      audio.play()
+    }
+  }
+}
+
+const bgmStart = async (retryTimes: number = 0) => {
+  const source = resources.bgmKey
+  if (!source) return
+
+  bgmAudio.src = source.src
+  bgmAudio.loop = true
+
+  try {
+    await bgmAudio.play()
+  } catch (err) {
+    console.log(err)
+    if (retryTimes < 5) {
+      setTimeout(() => {
+        bgmStart(retryTimes + 1)
+      }, 1000)
+    }
+  }
+}
+
+const bgmStop = () => {
+  bgmAudio.pause()
+  bgmAudio.src = ''
+}
+
+export default {
+  fire,
+  bgmStart,
+  bgmStop
+}
---
+++ src/utils/launchpad/resources.ts
@@ -0,0 +1,27 @@
+const resources = {
+  eat: {
+    src: 'assets/sound/eat.mp3',
+    times: 3
+  },
+  doorOpen: {
+    src: 'assets/sound/door_open.mp3',
+    times: 1
+  },
+  doorClose: {
+    src: 'assets/sound/door_close.mp3',
+    times: 1
+  },
+  chestClose: {
+    src: 'assets/sound/chest_close.mp3',
+    times: 1
+  },
+  chestOpen: {
+    src: 'assets/sound/chest_close.mp3',
+    times: 1
+  },
+  bgmKey: {
+    src: 'https://yiyi-hack.oss-cn-shenzhen.aliyuncs.com/soundtrack/key.mp3'
+  }
+}
+
+export default resources
